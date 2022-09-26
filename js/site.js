bd2536248332a05a3fa0caba28f80c9401eefe97
cg1image <54491781+cg1image@users.noreply.github.com>
2022-09-26 10:15:53 -0700

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,26 @@
+
+
+
+//get the values from the Page
+//starts or controller function
+function getValues(){
+    //get values from the page
+    let startValue = document.getElementById("startValue")
+
+
+    //we need to call generateNumbers
+
+    //we call displayNumbers
+}
+
+//generate numbers from startValue to the endValue
+//logic function(s)
+function gnerateNumbers(){
+
+}
+
+//dispay the numbers and mark even numbers bold
+//display or view functions
+function displayNumbers(){
+
+}
